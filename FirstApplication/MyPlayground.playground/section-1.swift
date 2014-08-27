c10cadcbb9db15858e5e70070d294c2a17d70f16
altock <sjs382@cornell.edu>
2014-08-26 23:12:07 -0400

    Stuff's happened

---
+++ FirstApplication/MyPlayground.playground/section-1.swift
@@ -0,0 +1,101 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let testFloat: Float = 4
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+let appleSummary = "I have \(width) apples"
+
+var list = [1,3,4]
+println(list[1])
+list[1]
+var dict = [1:"zero"]
+dict[1]
+println(dict[1])
+let visavis = "\(dict[1]) is the loneliest number"
+println(visavis)
+let emptyArray = [String]()
+let emptyDictionary = [:]
+let scores = [72,123,15,234,2]
+var teamScore = 0
+
+for score in scores {
+    if score > 50 {
+        teamScore += 3
+    }
+    else {
+        teamScore += 1
+    }
+}
+teamScore
+
+var optional: String? = "hi"
+optional == nil
+if let name = optional {
+    println("\(name) world!")
+}
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    let vegetableComment = "Add some raisins and make ants on a log."
+case "cucumber", "watercress":
+    let vegetableComment = "That would make a good tea sandwich."
+case let x where x.hasSuffix("pepper"):
+    let vegetableComment = "Is it a spicy \(x)?"
+default:
+    let vegetableComment = "Everything tastes good in soup."
+}
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+var Best = ""
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+            Best = kind
+        }
+    }
+}
+largest
+Best
+
+for i in 0..<4 {
+    println(i)
+}
+
+func kill(name:String)->String{
+    return "kill \(name)"
+}
+
+kill("bob")
+
+func average(numbers:Int...)->(Int){
+    var sum = 0
+    var num = 0
+    for i in numbers{
+        sum += i
+        num += 1
+    }
+    return sum/num
+}
+
+average(2,4,6)
+
+func makeIncrementer() -> (Int -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
--- orbital-clarity-669/main.py
+++ orbital-clarity-669/main.py
@@ -51,7 +51,7 @@ class Handler(webapp2.RequestHandler):
 
 	def render_str(self,template, **params):
 		t = jinja_env.get_template(template)
-		return t.render(params)
+		return t.render(**params)
 
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
@@ -61,11 +61,15 @@ class FizzBuzzHandler(Handler):
 		n = self.request.get('n', 0)
 		n= n and int(n)
 		self.render("fizzbuzz.html", n = n)
+class Art(db.Model):
+	title = db.StringProperty(required = True)
+	art = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
 
 class AsciiHandler(Handler):
 	def render_ascii(self, title="", art="",error=""):
 		arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
-		self.render("ascii.html", title=title, art=art, error=error)
+		self.render("ascii.html", title=title, art=art, error=error, arts = arts)
 
 	def get(self):
 		self.render_ascii()
@@ -83,10 +87,41 @@ class AsciiHandler(Handler):
 			error = "we need both a title and some artwork"
 			self.render_ascii(title,art, error=error)
 
-class Art(db.Model):
-	title = db.StringProperty(required = True)
-	art = db.TextProperty(required = True)
+class Blog(db.Model):
+	subject = db.StringProperty(required = True)
+	content = db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
+class BlogHandler(Handler):
+	def render_blog(self,error=""):
+		blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC")
+		self.render("blog.html", error=error, blogs = blogs)
+
+	def get(self):
+		self.render_blog()
+
+class FormHandler(Handler):
+	def render_form(self, subject="", content="",error=""):
+		self.render("form.html", subject=subject, content=content, error=error)
+
+	def get(self):
+		self.render_form()
+
+	def post(self):
+		subject = self.request.get("subject")
+		content = self.request.get("content")
+
+		if subject and content:
+			a = Blog(subject = subject, content = content)
+			a.put()
+			self.redirect("/blog/%d" % (a.key().id()))
+		else:
+			error = "we need both a subject and some content"
+			self.render_form(subject,content, error=error)
+
+class ProductHandler(Handler):
+	def get(self, iD):
+		blog = Blog.get_by_id(int(iD))
+		self.render("perma.html", blog = blog)
 
 
 class MainPage(webapp2.RequestHandler):
@@ -126,4 +161,4 @@ class SucessPage(webapp2.RequestHandler):
 		self.response.out.write("Welcome, " + q+"!")
 		#self.render('fizzbuzz.html')
 
-app = webapp2.WSGIApplication([('/',MainPage),('/welcome',SucessPage),('/fizzbuzz',FizzBuzzHandler),('/ascii',AsciiHandler)], debug=True)
+app = webapp2.WSGIApplication([('/',MainPage),('/welcome',SucessPage),('/fizzbuzz',FizzBuzzHandler),('/ascii',AsciiHandler),('/blog',BlogHandler),('/blog/(\d+)',ProductHandler),('/blog/newpost',FormHandler)], debug=True)
