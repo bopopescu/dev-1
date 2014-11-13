a0eedcf47e9b6d65bf698f401c93a2df146b3f72
altock <sjs382@cornell.edu>
2014-11-12 23:54:53 -0500

    Easy 40: non-loop printing

---
+++ easy/easy40.py
@@ -0,0 +1,12 @@
+__author__ = 'ssven'
+# easy40.py
+# Sam Svenningsen (sjs382@cornell.edu)
+# November 12, 2014
+
+"""Print all the numbers out between 1 and 1,000
+inclusive without using any loops or conditionals"""
+
+def p(n):
+    print n
+
+map(p,range(1,1001))
