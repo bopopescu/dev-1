13dc79927ad3ff684328ed7b36451f19f5e7dc3d
altock <sjs382@cornell.edu>
2017-02-16 13:56:45 -0500

    Total Reps script for KettleBug competition

---
+++ new/random_py/kb_users_script/reps_in_past_week.py
@@ -0,0 +1,36 @@
+import json, csv
+from datetime import datetime
+
+users = {}
+
+with open('project-8035943003290177832-KBSets-export.json') as kb_json_data:
+	d = json.load(kb_json_data)
+	for i in d:
+
+		user = d[i]["user"]
+		date = d[i]["date"]
+
+		if "KBExercises" not in d[i] or date <= 1486679820:
+			continue
+		exercises = d[i]["KBExercises"]
+		reps = 0
+		for exercise in exercises:
+			
+			reps += exercises[exercise]["reps"]
+
+		
+
+
+		print(reps)
+
+		users.setdefault(user, 0)
+		users[user] += reps
+
+users = sorted(list(users.items()), key=lambda x: -x[1])
+print(users)
+
+with open('rep_leaders.csv','w') as csvfile:
+	csv_writer = csv.writer(csvfile)
+	csv_writer.writerow(['Username','Total Reps'])
+	for user, total_reps in users:
+		csv_writer.writerow([user, total_reps])
