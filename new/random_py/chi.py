e247d626d9809b07e8ffcc8244a318c48e59e6a8
altock <sjs382@cornell.edu>
2017-03-27 23:05:48 -0400

    Chi Square update

--- new/random_py/chi.py
+++ new/random_py/chi.py
@@ -3,13 +3,22 @@ sig_level = 0.05
 
 data = [[200,150,50],[250,300,50]]
 
-E = [[0 for j in range(len(data[0]))] for i in range(len(data))]
 
 
-chi_squared = 0
-for i in range(len(data)):
-	for j in range(len(data[i])):
-		E[i][j] = (sum(data[i]) * (data[0][j] + data[1][j]))/ 1000
-		chi_squared += (data[i][j] - E[i][j])**2 / E[i][j]
 
-print(chi_squared)
+
+def chi_score(data):
+	E = [[0 for j in range(len(data[0]))] for i in range(len(data))]
+	chi_squared = 0
+	den = sum([sum(d) for d in data])
+
+	for i in range(len(data)):
+		d_i = sum(data[i])
+		for j in range(len(data[i])):
+			E[i][j] = (d_i * (data[0][j] + data[1][j]))/ den 
+
+			chi_squared += (data[i][j] - E[i][j])**2 / E[i][j]
+			
+	return chi_squared
+
+print(chi_score(data))
